1719a5d9a16758c9d963409cf0dc86ce169a903a
cactus8603 <cactus8603@users.noreply.github.com>
2020-06-28 05:15:44 +0800

    Check all function on cactus

--- api/config/config.php
+++ api/config/config.php
@@ -17,9 +17,10 @@
         // Database
         'host' => 'localhost',
         'username' => 'root',
+        'password' => '415646851',
         // 'host' => "140.122.184.132",
         // 'username' => 'team10',
-        'password' => 'DBehZktaWHEvdlY',
+        // 'password' => 'DBehZktaWHEvdlY',
         'dbname' => 'team10',
     );
 
